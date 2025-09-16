520fbb91d65f7dd254f57706cbab240f3d2f6618
Suhana783 <suhanachauhan133@gmail.com>
2025-09-16 16:08:11 +0530

    solved 2 string questions

---
+++ first.js
@@ -0,0 +1,37 @@
+//1.Reverse a String
+//Given a string, reverse it without using built-in reverse functions.
+
+// word = "hello"
+// reverse_word = ""
+
+// for (let i = 0; i < word.length; i++) {
+//     reverse_word = word[i] + reverse_word
+// }
+
+// console.log(reverse_word)
+
+
+// # 2.Check Palindrome
+// # Determine if a given string is a palindrome (reads the same forwards and backwards).
+
+function palindromeChecker (word) {
+    i = 0
+    j = word.length-1
+
+    while (i <= j && j < word.length) {
+
+        if (word[i] === word[j]) {
+            i ++
+            j --
+        }
+
+        else {
+            return "not a palindrome"
+        }
+  
+    }
+    return "is palindrome"  
+}
+
+word = "oarraq"
+console.log(palindromeChecker(word))
