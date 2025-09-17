07840614cbc1da10c5539e3c051eb00c5af821f5
Suhana783 <suhanachauhan133@gmail.com>
2025-09-17 16:29:25 +0530

    solved one question

--- first.js
+++ first.js
@@ -14,24 +14,50 @@
 // # 2.Check Palindrome
 // # Determine if a given string is a palindrome (reads the same forwards and backwards).
 
-function palindromeChecker (word) {
-    i = 0
-    j = word.length-1
+// function palindromeChecker (word) {
+//     i = 0
+//     j = word.length-1
 
-    while (i <= j && j < word.length) {
+//     while (i <= j && j < word.length) {
 
-        if (word[i] === word[j]) {
-            i ++
-            j --
-        }
+//         if (word[i] === word[j]) {
+//             i ++
+//             j --
+//         }
 
-        else {
-            return "not a palindrome"
-        }
+//         else {
+//             return "not a palindrome"
+//         }
   
+//     }
+//     return "is palindrome"  
+// }
+
+// word = "oarraq"
+// console.log(palindromeChecker(word))
+
+
+
+// 3. Find out non-repeating character in an array of integers 
+
+function nonRepeatingNumber(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let isUnique = true; 
+        for (let j = 0; j < arr.length; j++) {
+            if (i !== j && arr[i] === arr[j]) {
+                isUnique = false; 
+                break; 
+            }
+        }
+
+        if (isUnique) {
+            return arr[i];
+        }
     }
-    return "is palindrome"  
+    return null; 
 }
 
-word = "oarraq"
-console.log(palindromeChecker(word))
+ arr = [ 10, 10, 11, 12, 12 ];
+console.log(nonRepeatingNumber(arr)); 
+
+
